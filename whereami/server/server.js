2a9a0ae435460fa6cdfabcfa66c7e280e3083bc9
root4me <pixelstripes@gmail.com>
2016-04-06 21:33:38 -0500

    server subs for db operations

--- whereami/server/server.js
+++ whereami/server/server.js
@@ -12,6 +12,27 @@ app.get('/', function (req, res) {
   res.json({ status: 'alive' });
 });
 
+app.post('/location', function(req, res) {
+
+  var data = {
+    _id: 1,
+    longitude: "100.123456",
+    latitude: "-80.123456",
+    clientCreated: "04/06/2016 6:30:00 PM",
+    clientCaptured: "04/06/2016 6:29:00 PM",
+    created: "04/06/2016 6:40:00 PM";
+  };
+
+  dbUtils.save(data, function(err, result) {
+    if (!err) {
+      console.log(result);
+    }
+    else {
+      console.log(err);
+    }
+  });
+});
+
 app.listen(9000, function () {
   console.log('listening at http://localhost:9000');
 });
