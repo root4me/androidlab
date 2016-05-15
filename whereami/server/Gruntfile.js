6b7063628d47884a22faf8c6089d65c35dc0db0d
harish <pixelstripes@gmail.com>
2016-05-14 21:34:03 -0500

    styles for post

--- whereami/server/Gruntfile.js
+++ whereami/server/Gruntfile.js
@@ -11,180 +11,189 @@ module.exports = function(grunt) {
     app: 'app',
     dist: 'dist',
     srcjs: ['javascripts/*.*', '../bower_components/jquery/dist/jquery.js',
-    '../bower_components/foundation-sites/dist/foundation.js'
-  ],
-  srcscss: ['scss/*.*'],
-  srccss: ['stylesheets/*.*', '../bower_components/foundation-sites/dist/foundation.css'],
-  srcimg: ['images/*.*'],
-};
+      '../bower_components/foundation-sites/dist/foundation.js'
+    ],
+    srcscss: ['scss/*.*'],
+    srccss: ['stylesheets/*.*', '../bower_components/foundation-sites/dist/foundation.css'],
+    srcimg: ['images/*.*'],
+    srcarticles: ['articles/*.*'],
+  };
 
-// Define the configuration for all the tasks
-grunt.initConfig({
-  // Project settings
-  config: config,
-  pkg: grunt.file.readJSON('package.json'),
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    // Project settings
+    config: config,
+    pkg: grunt.file.readJSON('package.json'),
 
-  clean: {
-    dist: {
-      files: [{
-        dot: true,
-        src: ['.tmp', '<%= config.dist %>/*', '!<%= config.dist %>/.git*']
-      }]
-    }
-  },
-
-  processhtml: {
-    dist: {
-      files: [{
-        expand: true,
-        cwd: 'views',
-        src: ['**/*.handlebars'],
-        dest: '<%= config.dist %>/views',
-        ext: '.handlebars'
-      }],
-    },
-  },
-
-  copy: {
-    distimg: {
-      files: [{
-        expand: true,
-        cwd: 'public',
-        src: '<%= config.srcimg %>',
-        dest: '<%= config.dist %>/public',
-      }]
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: ['.tmp', '<%= config.dist %>/*', '!<%= config.dist %>/.git*']
+        }]
+      }
     },
 
-    appFiles: {
-      files: [{
-        expand: true,
-        src: ['app.js', 'config.js' , 'server.js', 'bower.json' , 'package.json', 'routes/**/*', 'data/**/*'],
-        dest: '<%= config.dist %>',
-      }]
-    },
-  },
-
-  concat: {
-    options: {
-      //separator: ';',
-    },
-    dist: {
-      src: ['public/javascripts/scrollnfade.js', 'public/javascripts/home.js'],
-      dest: 'public/javascripts/site.js',
-    },
-  },
-
-  uglify: {
-    options: {
-
-    },
-    dist: {
-      files: [{
-        expand: true,
-        flatten: true,
-        extDot: 'last',
-        /* Without this jquery.xyz.js and jquery.js at source will only produce one jquery.min.js at the destination folder
-        Drove me nuts wondering why uglify seem to miss files. Turns out, i didnt read the doc close enough.
-        But, why some one thought that setting extDot default to 'first' is the best thing to do is still a mystery. Extensions usually start after the last dot !  */
-        cwd: 'public',
-        src: '<%= config.srcjs %>',
-        dest: '<%= config.dist %>/public/javascripts',
-        ext: '.js'
-      }]
-    },
-  },
-  cssmin: {
-    options: {
-      processImport: false,
-    },
-    dist: {
-      files: [{
-        expand: true,
-        flatten: true,
-        cwd: 'public',
-        src: '<%= config.srccss %>',
-        dest: '<%= config.dist %>/public/stylesheets',
-        ext: '.min.css'
-      }]
-    }
-  },
-
-  sass: {
-    options: {
-      sourceMap: true,
-      style: 'expanded'
-    },
-    dist: {
-      files: [{
-        expand: true,
-        cwd: 'scss',
-        src: ['*.scss'],
-        dest: 'public/stylesheets',
-        ext: '.css'
+    processhtml: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'views',
+          src: ['**/*.handlebars'],
+          dest: '<%= config.dist %>/views',
+          ext: '.handlebars'
+        }],
       },
-    ]
-    }
-  },
-  htmlmin: {
-    dist: {
+    },
+
+    copy: {
+      distimg: {
+        files: [{
+          expand: true,
+          cwd: 'public',
+          src: '<%= config.srcimg %>',
+          dest: '<%= config.dist %>/public',
+        }]
+      },
+
+      distarticles: {
+        files: [{
+          expand: true,
+          cwd: 'public',
+          src: '<%= config.srcarticles %>',
+          dest: '<%= config.dist %>/public',
+        }]
+      },
+
+      appFiles: {
+        files: [{
+          expand: true,
+          src: ['app.js', 'config.js', 'server.js', 'bower.json', 'package.json', 'routes/**/*', 'data/**/*'],
+          dest: '<%= config.dist %>',
+        }]
+      },
+    },
+
+    concat: {
       options: {
-        removeComments: true,
-        collapseWhitespace: true
+        //separator: ';',
       },
-      files: [{
-        expand: true,
-        cwd: '<%= config.dist %>',
-        src: '**/*.handlebars',
-        dest: '<%= config.dist %>',
-      }]
+      dist: {
+        src: ['public/javascripts/scrollnfade.js', 'public/javascripts/home.js'],
+        dest: 'public/javascripts/site.js',
+      },
     },
 
-  },
-  watch: {
-    options: {
-      livereload: true
+    uglify: {
+      options: {
+
+      },
+      dist: {
+        files: [{
+          expand: true,
+          flatten: true,
+          extDot: 'last',
+          /* Without this jquery.xyz.js and jquery.js at source will only produce one jquery.min.js at the destination folder
+          Drove me nuts wondering why uglify seem to miss files. Turns out, i didnt read the doc close enough.
+          But, why some one thought that setting extDot default to 'first' is the best thing to do is still a mystery. Extensions usually start after the last dot !  */
+          cwd: 'public',
+          src: '<%= config.srcjs %>',
+          dest: '<%= config.dist %>/public/javascripts',
+          ext: '.js'
+        }]
+      },
     },
-    scss: {
-      files: 'scss/*',
-      tasks: ['sass']
+    cssmin: {
+      options: {
+        processImport: false,
+      },
+      dist: {
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: 'public',
+          src: '<%= config.srccss %>',
+          dest: '<%= config.dist %>/public/stylesheets',
+          ext: '.min.css'
+        }]
+      }
     },
+
+    sass: {
+      options: {
+        sourceMap: true,
+        style: 'expanded'
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['*.scss'],
+          dest: 'public/stylesheets',
+          ext: '.css'
+        }, ]
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.dist %>',
+          src: ['**/*.handlebars','**/*.html'],
+          dest: '<%= config.dist %>',
+        }]
+      },
+
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      scss: {
+        files: 'scss/*',
+        tasks: ['sass']
+      },
+      express: {
+        files: ['public/**/*.js', 'views/**/*.handlebars', '*.js', 'data/**/*.js', 'routes/**/*.js'],
+        tasks: ['express:dev'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
     express: {
-      files: ['public/**/*.js', 'views/**/*.handlebars', '*.js', 'data/**/*.js', 'routes/**/*.js'],
-      tasks: ['express:dev'],
       options: {
-        spawn: false
+
+      },
+      dev: {
+        options: {
+          script: 'server.js',
+        }
       }
-    }
-  },
-
-  express: {
-    options: {
-
     },
-    dev: {
-      options: {
-        script: 'server.js',
-      }
-    }
-  },
-});
+  });
 
-//grunt.registerTask('default', ['clean:dist', 'copy:srcimg', 'copy:distimg', 'processhtml', 'cssmin', 'uglify', 'htmlmin']);
+  //grunt.registerTask('default', ['clean:dist', 'copy:srcimg', 'copy:distimg', 'processhtml', 'cssmin', 'uglify', 'htmlmin']);
 
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-server');
 
-grunt.registerTask('default', ['usage']);
-grunt.registerTask('dev', ['sass', 'express', 'watch']);
-grunt.registerTask('build', ['clean:dist','sass', 'copy', 'processhtml', 'cssmin', 'uglify', 'htmlmin']);
+  grunt.registerTask('default', ['usage']);
+  grunt.registerTask('dev', ['sass', 'express', 'watch']);
+  grunt.registerTask('build', ['clean:dist', 'sass', 'copy', 'processhtml', 'cssmin', 'uglify', 'htmlmin']);
 
-grunt.registerTask('usage', 'display usage parameters', function() {
-  console.log("usage :");
-  console.log("\t grunt clean:dist - cleans /dist folder");
-  console.log("\t grunt dev - development mode");
-  console.log("\t grunt build - build and update minimized version to /dist folder");
-});
+  grunt.registerTask('usage', 'display usage parameters', function() {
+    console.log("usage :");
+    console.log("\t grunt clean:dist - cleans /dist folder");
+    console.log("\t grunt dev - development mode");
+    console.log("\t grunt build - build and update minimized version to /dist folder");
+  });
 
-// add sass to the grunt file
+  // add sass to the grunt file
 
 };
