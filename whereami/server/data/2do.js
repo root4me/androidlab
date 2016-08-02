a290a86f06bf173bf26b219ac5a2788fe270500e
harish <pixelstripes@gmail.com>
2016-08-01 21:56:04 -0500

    sorted 2do . Done tasks are moved to a new collection

--- whereami/server/data/2do.js
+++ whereami/server/data/2do.js
@@ -1,37 +1,51 @@
 var mh = require('./mongodbHelper');
 var config = require('../config');
 
-var getAll = function(callback){
-  mh.getAll(config.database.server,config.database.port,config.database.tasksDb,'tasks', function(err,data){
-    callback(err,data);
+var getAll = function(callback) {
+  mh.getAllSorted(config.database.server, config.database.port, config.database.tasksDb, 'tasks', {
+    task: 1
+  }, function(err, data) {
+    callback(err, data);
   });
 };
 
-var insert = function(data, callback){
-
-  mh.insert(config.database.server,config.database.port,config.database.tasksDb,'tasks',data, function(err,data){
-    if (!(err))
-    {
-      callback(err,data.ops[0]);
+var insert = function(data, callback) {
+  mh.insert(config.database.server, config.database.port, config.database.tasksDb, 'tasks', data, function(err, data) {
+    if (!(err)) {
+      callback(err, data.ops[0]);
+    } else {
+      callback(err, data);
     }
-    else {
-      callback(err,data);
+  });
+};
+
+var update = function(id, data, callback) {
+
+  mh.update(config.database.server, config.database.port, config.database.tasksDb, 'tasks', id, data, function(err, data) {
+    callback(err, data);
+  });
+};
+
+var del = function(id, callback) {
+  mh.getById(config.database.server, config.database.port, config.database.tasksDb, 'tasks', id, function(err, data) {
+    if (!(err)) {
+      // move task created date into a different field
+      data[0].taskcreated = data[0].created;
+      console.log(data);
+      mh.insert(config.database.server, config.database.port, config.database.tasksDb, 'tasksDone', data[0], function(err, data) {
+        if (!(err)) {
+          mh.delete(config.database.server, config.database.port, config.database.tasksDb, 'tasks', id, function(err, data) {
+            //    console.log(data);
+            callback(err, data);
+          });
+        } else {
+          callback(err, data);
+        }
+      });
+
+    } else {
+      callback(err, data);
     }
-  });
-};
-
-var update = function(id, data, callback){
-
-  mh.update(config.database.server,config.database.port,config.database.tasksDb,'tasks', id, data, function(err,data){
-    callback(err,data);
-  });
-};
-
-var del = function(id, callback){
-
-  mh.delete(config.database.server,config.database.port,config.database.tasksDb,'tasks',id, function(err,data){
-//    console.log(data);
-    callback(err,data);
   });
 };
 
--- whereami/server/data/mongodbHelper.js
+++ whereami/server/data/mongodbHelper.js
@@ -1,103 +1,170 @@
 /**
-* Generic CRUD calls to mongodb using node js mongodb driver
-* package.json need to have "mongodb": "^2.1.16",
-**/
+ * Generic CRUD calls to mongodb using node js mongodb driver
+ * package.json need to have "mongodb": "^2.1.16",
+ **/
 
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectID = require('mongodb').ObjectID;
 
-var url = function(server,port,dbname){
+var url = function(server, port, dbname) {
   return 'mongodb://' + server + ':' + port + '/' + dbname;
 };
 
 /**
-* [function description]
-* @param  {String}   server         [description]
-* @param  {Number}   port           [description]
-* @param  {[type]}   dbName         [description]
-* @param  {[type]}   collectionName [description]
-* @param  {Function} callback       [description]
-* @return {[type]}                  [description]
-*/
-var getAll = function(server,port, dbName, collectionName, callback){
+ * [function description]
+ * @param  {String}   server         [description]
+ * @param  {Number}   port           [description]
+ * @param  {[type]}   dbName         [description]
+ * @param  {[type]}   collectionName [description]
+ * @param  {Function} callback       [description]
+ * @return {[type]}                  [description]
+ */
+var getAll = function(server, port, dbName, collectionName, callback) {
 
-  mongo.connect(url(server,port, dbName), function(err, db) {
+  mongo.connect(url(server, port, dbName), function(err, db) {
 
-    if (err != null)
-    {
-      callback(err,null);
+    if (err !== null) {
+      callback(err, null);
       return;
     }
-//    console.log("Connected to mongodb server");
+    //    console.log("Connected to mongodb server");
     var collection = db.collection(collectionName);
 
     collection.find().toArray(function(err, docs) {
       db.close();
-      callback(err,docs);
+      callback(err, docs);
     });
   });
 
 };
 
 /**
-* [function description]
-* @param  {[type]}   server         [description]
-* @param  {[type]}   port           [description]
-* @param  {[type]}   dbName         [description]
-* @param  {[type]}   collectionName [description]
-* @param  {[type]}   data           [description]
-* @param  {Function} callback       [description]
-* @return {[type]}                  [description]
-*/
-var insert = function(server, port, dbName, collectionName, data, callback){
+ * [function description]
+ * @param  {[type]}   server         [description]
+ * @param  {[type]}   port           [description]
+ * @param  {[type]}   dbName         [description]
+ * @param  {[type]}   collectionName [description]
+ * @param  {[type]}   sortBy         Pass in { field : 1 } for sorting the result by a specific field
+ * @param  {Function} callback       [description]
+ * @return {[type]}                  [description]
+ */
+var getAllSorted = function(server, port, dbName, collectionName, sortBy, callback) {
 
-  mongo.connect(url(server,port, dbName), function(err, db) {
-//    console.log("Connected correctly to server");
+  mongo.connect(url(server, port, dbName), function(err, db) {
+
+    if (err !== null) {
+      callback(err, null);
+      return;
+    }
+    //console.log("Connected to mongodb server");
     var collection = db.collection(collectionName);
 
-    data.created = Date();
-//    console.log(data);
+    //console.log(sortBy);
 
-    collection.insertOne(data , function(err,docs){
-      callback(err,docs);
+    collection.find().sort(sortBy).toArray(function(err, docs) {
+      db.close();
+      callback(err, docs);
     });
   });
+
 };
 
-var update = function(server, port, dbName, collectionName, id, data, callback){
-  mongo.connect(url(server,port, dbName), function(err, db) {
-//    console.log("Connected correctly to server for update");
-    var collection = db.collection(collectionName);
+/**
+ * [function description]
+ * @param  {[type]}   server         [description]
+ * @param  {[type]}   port           [description]
+ * @param  {[type]}   dbName         [description]
+ * @param  {[type]}   collectionName [description]
+ * @param  {[type]}   id             [description]
+ * @param  {Function} callback       [description]
+ * @return {[type]}                  [description]
+ */
+var getById = function(server, port, dbName, collectionName, id, callback) {
 
-    data.updated = Date();
-//    console.log(data);
+  mongo.connect(url(server, port, dbName), function(err, db) {
 
-    collection.update(
-      { '_id' : ObjectID(id) },
-      { $set: data },
-      function(err,docs){
-        callback(err,docs);
-    });
-  });
-};
-
-var del = function(server, port, dbName, collectionName, id, callback){
-
-  mongo.connect(url(server,port, dbName), function(err, db) {
-//    console.log("Connected correctly to server for delete");
+    if (err !== null) {
+      callback(err, null);
+      return;
+    }
+    //    console.log("Connected to mongodb server");
     var collection = db.collection(collectionName);
     var objid = new ObjectID(id);
 
-    collection.deleteOne({_id: ObjectID(id)},function(err, docs) {
-//      console.log(docs);
-//      console.log(err);
-      callback(err,docs);
+    collection.find({
+      _id: ObjectID(id)
+    }).toArray(function(err, docs) {
+      db.close();
+      callback(err, docs);
+    });
+  });
+};
+
+/**
+ * [function description]
+ * @param  {[type]}   server         [description]
+ * @param  {[type]}   port           [description]
+ * @param  {[type]}   dbName         [description]
+ * @param  {[type]}   collectionName [description]
+ * @param  {[type]}   data           [description]
+ * @param  {Function} callback       [description]
+ * @return {[type]}                  [description]
+ */
+var insert = function(server, port, dbName, collectionName, data, callback) {
+
+  mongo.connect(url(server, port, dbName), function(err, db) {
+    //    console.log("Connected correctly to server");
+    var collection = db.collection(collectionName);
+
+    data.created = Date();
+        console.log(data);
+
+    collection.insertOne(data, function(err, docs) {
+      callback(err, docs);
+    });
+  });
+};
+
+var update = function(server, port, dbName, collectionName, id, data, callback) {
+  mongo.connect(url(server, port, dbName), function(err, db) {
+    //    console.log("Connected correctly to server for update");
+    var collection = db.collection(collectionName);
+
+    data.updated = Date();
+    //    console.log(data);
+
+    collection.update({
+        '_id': ObjectID(id)
+      }, {
+        $set: data
+      },
+      function(err, docs) {
+        callback(err, docs);
+      });
+  });
+};
+
+var del = function(server, port, dbName, collectionName, id, callback) {
+
+  mongo.connect(url(server, port, dbName), function(err, db) {
+    //    console.log("Connected correctly to server for delete");
+    var collection = db.collection(collectionName);
+    var objid = new ObjectID(id);
+
+    collection.deleteOne({
+      _id: ObjectID(id)
+    }, function(err, docs) {
+      //      console.log(docs);
+      //      console.log(err);
+      callback(err, docs);
     });
   });
 };
 
 module.exports.getAll = getAll;
+module.exports.getAllSorted = getAllSorted;
+module.exports.getById = getById;
 module.exports.insert = insert;
 module.exports.update = update;
 module.exports.delete = del;
